e97724da65805e5713659d4c83cb0fc94c010196
cspags <cspags@gmail.com>
2013-07-11 20:36:29 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Randolunch::Application.config.secret_token = 'b05510aaa643d31e0276b3c9f441c5d7dbee156a9fa6667bc7c80fdf758cf9cfa59474867c17732edb2a7ac917599b8f94e9d1a218cf811024dc0f41f14e98b5'
